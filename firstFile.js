6a1ba1b0959b95c531ceb3f8d9fc664b35b5903b
Musahaf Ali <musahafali43@gmail.com>
2026-03-26 08:33:15 +0500

    added all the code

--- firstFile.js
+++ firstFile.js
@@ -6,6 +6,10 @@ const reducer = (store = INITIAL_VALUE , action)=>{
     let newStore = store;
     if(action.type === 'INCREMENT'){
         newStore =  {counter: store.counter + 1};
+    }else if (action.type === 'DECREMENT'){
+        newStore =  {counter: store.counter - 1};
+    }else if(action.type === 'ADD' ){
+        newStore =  {counter: store.counter + action.payload.number};
     }
     return newStore;
 }
@@ -19,3 +23,5 @@ console.log(state);
 store.subscribe(subscriber)
 
 store.dispatch({type:'INCREMENT'})
+store.dispatch({type:'DECREMENT'})
+store.dispatch({type:'ADD' , payload:{number : 7}})
